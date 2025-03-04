168c55e5118ab45113c8bcc7f3c4adc0336b3985
Loris <farrauto.loris@gmail.com>
2025-03-04 12:48:54 +0100

    First commit

---
+++ src/main/java/com/advancia/chat4me_api_gateway/Chat4MeApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.advancia.chat4me_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat4MeApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chat4MeApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/advancia/chat4me_api_gateway/Chat4MeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advancia.chat4me_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat4MeApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
